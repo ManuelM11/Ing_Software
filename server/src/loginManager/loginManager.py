8bd5fe97ee8a2181c5ff7c36b73757e741433206
NicolasAraya932 <nicoarayapcap@gmail.com>
2023-06-11 20:58:25 -0400

    Test

--- server/src/loginManager/loginManager.py
+++ server/src/loginManager/loginManager.py
@@ -1,6 +1,6 @@
 from flask_login import LoginManager
 
-def login_required(role="ANY"):
+def login_required(role="PACIENTE"):
     def wrapper(fn):
         @wraps(fn)
         def decorated_view(*args, **kwargs):
@@ -8,7 +8,7 @@ def login_required(role="ANY"):
             if not current_user.is_authenticated():
                return current_app.login_manager.unauthorized()
             urole = current_app.login_manager.reload_user().get_urole()
-            if ( (urole != role) and (role != "ANY")):
+            if ( (urole != role) and (role != "PACIENTE")):
                 return current_app.login_manager.unauthorized()      
             return fn(*args, **kwargs)
         return decorated_view
