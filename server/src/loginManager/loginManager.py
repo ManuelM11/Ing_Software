10939599b4360d4e3d91c719f1b17227eeb77d68
NicolasAraya932 <nicoarayapcap@gmail.com>
2023-06-11 20:54:40 -0400

    Adding log in system (test)

---
+++ server/src/loginManager/loginManager.py
@@ -0,0 +1,15 @@
+from flask_login import LoginManager
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+
+            if not current_user.is_authenticated():
+               return current_app.login_manager.unauthorized()
+            urole = current_app.login_manager.reload_user().get_urole()
+            if ( (urole != role) and (role != "ANY")):
+                return current_app.login_manager.unauthorized()      
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
