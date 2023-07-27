cd71470e58cb7dba3279877113107acda296e1d0
NicolasAraya932 <nicoarayapcap@email.com>
2023-07-26 20:56:52 -0400

    Nuevas Vistas: Soporte, ContactarMedico, Configuracion

--- Ing_Software/MyProject/user_app/App.js
+++ Ing_Software/MyProject/user_app/App.js
@@ -13,6 +13,9 @@ import Login from './screens/Login';
 import Asistencia from './screens/Asistencia';
 import AutoExamen from './screens/AutoExamen';
 import PatientMenu from './screens/PatientMenu';
+import Configuracion from './screens/Configuracion';
+import Contactar from './screens/Contactar';
+import Soporte from './screens/Soporte';
 
 // Styles
 import styles from './styles/styles';
@@ -23,7 +26,7 @@ const { width, height } = Dimensions.get('window');
 const Stack = createNativeStackNavigator();
 
 // URL DE FETCH
-const URL = 'https://b890-190-95-120-224.ngrok-free.app/fetchPatients';
+const URL = 'https://4c01-190-95-120-224.ngrok-free.app/fetchPatients';
 
 /*
 Mencionar que NavigationContainer corresponde al headbar, por lo tanto, puede modificarse la vista de este en 
@@ -42,18 +45,35 @@ export default function App() {
   const handleModalClose = () => {
     setModalVisible(false);
   };
+
   const any = 0;
 
-  // Corresponde a la vista de las opciones del usuario (Ubidado en HeadBar)
-  const MODAL = ({any}) =>{
+  // Corresponde a la vista de las opciones del usuario (Ubicado en HeadBar)
+  const MODAL = ({ navigation }) =>{
+
+    const toConfig = () => {
+      setModalVisible(false);
+      navigation.navigate('Configuracion');
+    };
+
+    const toMedic = () => {
+      setModalVisible(false);
+      navigation.navigate('ContactarMedico');
+    };
+
+    const toSupport = () => {
+      setModalVisible(false);
+      navigation.navigate('Soporte');
+    };
+
     return (
       <Modal visible={modalVisible} animationType='slide' transparent={true} onRequestClose={handleModalClose}>
         <View style={styles.modalContainer}>
           <Button title="Cerrar" onPress={handleModalClose} />
           <View style={styles.dropdownContainer}>
-            <Button title="Configuración"/>
-            <Button title="Contactar Médico"/>
-            <Button title="Soporte"/>
+            <Button title="Configuración" onPress={toConfig}/>
+            <Button title="Contactar Médico" onPress={toMedic}/>
+            <Button title="Soporte" onPress={toSupport}/>
           </View>
         </View>
       </Modal>
@@ -64,7 +84,11 @@ export default function App() {
     // NAVIGATION CONTROL && ROUTES VISUALS
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen 
+        <Stack.Screen 
+        name='MODAL'
+        component={MODAL}
+        />
+        <Stack.Screen 
           name="Login"
           component={Login}
           initialParams={{ url: URL }}
@@ -73,52 +97,106 @@ export default function App() {
         <Stack.Screen name="PatientMenu"
          component={PatientMenu}
          initialParams={{ url: URL }}
-         options={{
+         options={({ navigation }) => ({
           headerTitle: () => (
             <View style={styles.container}>
-                <View style={styles.headBar}>
-                  <View style={minSalLogo.logoLeft} />
-                  <View style={minSalLogo.logoRight} />
-                  <Text style={generalStyles.titles}>MPC</Text>
+              <View style={styles.headBar}>
+                <View style={minSalLogo.logoLeft} />
+                <View style={minSalLogo.logoRight} />
+                <Text style={generalStyles.titles}>MPC</Text>
                 <Button title="Menu" style={styles.button} onPress={handleButtonPress}/>
-                </View>
-                <View style={{position: 'relative', left: width * 0.28}}>
-                  <MODAL onPress={handleModalClose} />
-                </View>
-            </View>),
-         }} 
+              </View>
+              <View style={{position: 'relative', left: width * 0.28}}>
+                <MODAL navigation={navigation} onPress={handleModalClose} />
+              </View>
+            </View>
+          ),
+        })} 
          />
         <Stack.Screen name="AutoExamen"
           component={AutoExamen}
-          options={{
+          options={({ navigation }) => ({
             headerTitle: () => (
               <View style={styles.container}>
-                  <View style={styles.headBar2}>
-                    <View style={minSalLogo.logoLeft} />
-                    <View style={minSalLogo.logoRight} />
-                    <Text style={generalStyles.titles}>Autoexamen</Text>
-                  </View>
-                  <View style={{position: 'relative', left: width * 0.28}}>
-                    <MODAL onPress={any} />
-                  </View>
-              </View>),
-           }} 
+                <View style={styles.headBar}>
+                  <View style={minSalLogo.logoLeft} />
+                  <View style={minSalLogo.logoRight} />
+                  <Text style={[generalStyles.titles, {position: 'absolute', padding: 16}]}>AutoExamen</Text>
+                </View>
+                <View style={{position: 'relative', left: width * 0.28}}>
+                  <MODAL navigation={navigation} onPress={handleModalClose} />
+                </View>
+              </View>
+            ),
+          })} 
           />
           <Stack.Screen name="Asistencia"
           component={Asistencia}
-          options={{
+          options={({ navigation }) => ({
             headerTitle: () => (
               <View style={styles.container}>
-                  <View style={styles.headBar2}>
-                    <View style={minSalLogo.logoLeft} />
-                    <View style={minSalLogo.logoRight} />
-                    <Text style={generalStyles.titles}>Asistencia</Text>
-                  </View>
-                  <View style={{position: 'relative', left: width * 0.28}}>
-                    <MODAL onPress={any} />
-                  </View>
-              </View>),
-           }} 
+                <View style={styles.headBar}>
+                  <View style={minSalLogo.logoLeft} />
+                  <View style={minSalLogo.logoRight} />
+                  <Text style={[generalStyles.titles, {position: 'absolute', padding: 16}]}>Asistencia</Text>
+                </View>
+                <View style={{position: 'relative', left: width * 0.28}}>
+                  <MODAL navigation={navigation} onPress={handleModalClose} />
+                </View>
+              </View>
+            ),
+          })} 
+          />
+          <Stack.Screen name="Configuracion"
+          component={Configuracion}
+          options={({ navigation }) => ({
+            headerTitle: () => (
+              <View style={styles.container}>
+                <View style={styles.headBar}>
+                  <View style={minSalLogo.logoLeft} />
+                  <View style={minSalLogo.logoRight} />
+                  <Text style={[generalStyles.titles, {position: 'absolute', padding: 16}]}>Configuración</Text>
+                </View>
+                <View style={{position: 'relative', left: width * 0.28}}>
+                  <MODAL navigation={navigation} onPress={handleModalClose} />
+                </View>
+              </View>
+            ),
+          })} 
+          />
+          <Stack.Screen name="ContactarMedico"
+          component={Contactar}
+          options={({ navigation }) => ({
+            headerTitle: () => (
+              <View style={styles.container}>
+                <View style={styles.headBar}>
+                  <View style={minSalLogo.logoLeft} />
+                  <View style={minSalLogo.logoRight} />
+                  <Text style={[generalStyles.titles, {position: 'absolute', padding: 16}]}>Contactar Médico</Text>
+                </View>
+                <View style={{position: 'relative', left: width * 0.28}}>
+                  <MODAL navigation={navigation} onPress={handleModalClose} />
+                </View>
+              </View>
+            ),
+          })} 
+          />
+          <Stack.Screen name="Soporte"
+          component={Soporte}
+          options={({ navigation }) => ({
+            headerTitle: () => (
+              <View style={styles.container}>
+                <View style={styles.headBar}>
+                  <View style={minSalLogo.logoLeft} />
+                  <View style={minSalLogo.logoRight} />
+                  <Text style={generalStyles.titles}>Soporte</Text>
+                </View>
+                <View style={{position: 'relative', left: width * 0.28}}>
+                  <MODAL navigation={navigation} onPress={handleModalClose} />
+                </View>
+              </View>
+            ),
+          })} 
           />
       </Stack.Navigator>
     </NavigationContainer>
